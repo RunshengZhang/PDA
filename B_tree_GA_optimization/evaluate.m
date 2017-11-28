354e1bb05d801b9d7f8b7b6f247ad47ffba61ee3
Maytheforcebewithmycode <32641709+Maytheforcebewithmycode@users.noreply.github.com>
2017-11-28 08:56:37 -0600

    Back to old version

--- B_tree_GA_optimization/evaluate.m
+++ B_tree_GA_optimization/evaluate.m
@@ -2,34 +2,28 @@
 % Yunyi
 % Oct 30
 
-% Update: in order to fit in the new struct of trees~
-% Runsheng
-% Nov 27
-
 function [ area, hpwl ] = evaluate( placement, block, net )
 
-[block_number,~] = size(block);
-NP = size(fieldnames(placement),1);             % fieldnames return the n x 1 array   
-net_name    = fieldnames(net);                  %   Net: struct  net_name is an array
+[block_number,~,NP] = size(placement);          %   placement: 3-D matrix
+net_name    = fieldnames(net);                  %   Net: struct
 net_number  = length(net_name);
 area        = zeros(1,NP);
 hpwl        = zeros(1,NP);
 
 for n = 1:NP
-    
-    % build the field name
-    name{n} = sprintf('NP%d', n);
-
-    cur_placement = placement.(name{n});
-
     x_max       = zeros(1, block_number);           %   for computing area
     y_max       = zeros(1, block_number);
     w           = zeros(1, net_number);             %   for computing HPWL
     l           = zeros(1, net_number);
 
+    %   1. Sort Placement
+    for i = 1:block_number
+        placement_ordered(i,1:2,n) = placement(find(placement(:,1,n)==i),2:3,n);
+    end
+
     %   2. Compute Area
-    x_max = cur_placement(:,1) + cur_placement(:,3);    % NumberOfBlock x 1 array
-    y_max = cur_placement(:,2) + cur_placement(:,4);
+    x_max = placement_ordered(:,1,n) + str2double(block(:,2));
+    y_max = placement_ordered(:,2,n) + str2double(block(:,3));
     x = max(x_max);
     y = max(y_max);
     area(n) = x*y;
@@ -38,8 +32,8 @@ for n = 1:NP
     for i = 1:net_number
         field = char(net_name(i));
         index = net.(field);
-        center_x = cur_placement(index,1) + 0.5*cur_placement(index,3);
-        center_y = cur_placement(index,2) + 0.5*cur_placement(index,4);
+        center_x = placement_ordered(index,1,n) + 0.5*str2double(block(index,2));
+        center_y = placement_ordered(index,2,n) + 0.5*str2double(block(index,3));
         w(i) = max(center_x) - min(center_x);
         l(i) = max(center_y) - min(center_y);
     end
