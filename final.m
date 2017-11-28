37d3558955908c180e69b7d92df54a267ec14980
RunshengZhang <runshengzhang@yahoo.com>
2017-11-28 16:26:55 -0600

    create the final function

---
+++ final.m
@@ -0,0 +1,5 @@
+% Final function used to wrap up the result 
+% Runsheng Zhang
+% Nov 28(Happy Thanksgiving)(The first Tue after Thanksgiving!)
+
+function [ area, hpwl ] = evaluate( placement, block, net )
